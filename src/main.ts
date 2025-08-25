4b20cfc5925a62061524af398bdf91406c232ba9
Vasyl <vasyl.hryha@epicmax.co>
2025-08-25 18:22:01 +0300

    - feat: init vue

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,11 @@
+import './assets/main.css'
+
 import { createApp } from 'vue'
-import { createRouter, createWebHistory, type RouteRecordRaw } from 'vue-router'
 import App from './App.vue'
-import HelloWorld from './components/HelloWorld.vue'
+import router from './router'
 
-const routes: RouteRecordRaw[] = [
-    { path: '/', component: HelloWorld },
-    { path: '/about', component: { template: '<div>About page</div>' } }
-]
+const app = createApp(App)
 
-const router = createRouter({
-    history: createWebHistory(),
-    routes
-})
+app.use(router)
 
-createApp(App).use(router).mount('#app')
+app.mount('#app')
--- vitetest.config.ts
+++
@@ -1,6 +0,0 @@
-import { defineConfig } from 'vitest/config'
-export default defineConfig({
-    test: {
-        coverage: { reporter: ['text', 'lcov'] } // creates coverage/lcov.info
-    }
-})
