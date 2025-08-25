f974cd2e58401f26898ea55883fd73278f4cebcc
Vasyl <vasyl.hryha@epicmax.co>
2025-08-25 14:56:51 +0300

    - feat: update lint and vitetest

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,10 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
-  plugins: [vue()]
+    test: {
+        coverage: {
+            provider: 'v8',
+            reporter: ['text', 'lcov'] // Sonar reads coverage/lcov.info
+        }
+    }
 })
